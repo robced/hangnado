846ba8c575f1353058a6aa6fd1a313083708594a
robced <117061812+robced@users.noreply.github.com>
2023-01-07 16:47:27 -0800

    feat: add auth file

---
+++ pages/auth.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Auth = () => {
+    // TODO: Create Authentication System with NextAuth.
+    return (
+        <div>
+            Login
+        </div>
+    );
+};
+
+export default Auth;
