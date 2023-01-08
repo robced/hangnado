8327d785c85850d42b0e813f2fefd918c58f32c5
robced <117061812+robced@users.noreply.github.com>
2023-01-07 16:21:09 -0800

    initial commit

---
+++ TODO.md
@@ -0,0 +1,13 @@
+1. Authentication.
+2. Socket.io Instances.
+3. Chats.
+4. Profile.
+5. Friends.
+6. Leaderboards.
+7. Help.
+8. Components
+    - Header.
+    - Navigation.
+    - Chat.
+    - Multiplayer.
+    - Get Updates.
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hangnado</title>
+        <meta name="description" content="Web-based word game." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="">
+      </main>
+    </>
+  )
+}
